d60dd62fa5cd51eabb43e90534deab270d8d7c7f
aXMediaSolutions <axmediasolutions@gmail.com>
2016-08-26 20:38:23 +1000

    Add project files.

---
+++ webapp1/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace webapp1.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ webapp1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webapp1.Startup))]
+namespace webapp1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
